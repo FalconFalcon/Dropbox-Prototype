4afc7113dbcd3333f8fde031ff8bc0862b37f994
FalconFalcon <adithyakarlapalem@gmail.com>
2017-11-07 22:14:44 -0800

    Login and SignUp

--- BackEnd/routes/users.js
+++ BackEnd/routes/users.js
@@ -61,4 +61,71 @@ var users = [
 ];
 
 
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/doLogin', function(req, res, next) {
+  var reqUsername = req.body.username;
+  var reqPassword = req.body.password;
+
+  if (reqUsername && reqPassword) {
+    //checking is the username is found in mongodb or Not
+    User.find({ username: reqUsername }, function(err, results) {
+      console.log('the results are' + results);
+      if (err) console.log(err);
+      else if (results.length == 0) {
+        res
+          .status(401)
+          .json({ message: 'Login Failed. Incorrect username or password' });
+      } else if (results[0].password == reqPassword) {
+        res.status(201).json({ message: 'Login successful' });
+      } else {
+        res
+          .status(401)
+          .json({ message: 'Login Failed. Incorrect username or password' });
+      }
+    });
+  } else {
+    console.log('The user entered nothing. Sending 500 Status to Front End');
+    res.status(500).json({ message: 'Enter a username and password' });
+  }
+});
+
+router.post('/doSignUp', function(req, res, next) {
+  var reqUsername = req.body.username;
+  var reqPassword = req.body.password;
+
+  console.log('Request to register username: ' + reqUsername);
+  console.log('Password to register username: ' + reqPassword);
+
+  if (reqUsername && reqPassword) {
+    try {
+      User.find({ username: reqUsername }, function(err, results) {
+        if (err) console.log(err);
+        else if (results.length != 0) {
+          res.status(401).json({ message: 'Already signed up' });
+        } else {
+          var newuser = new User({
+            username: reqUsername,
+            password: reqPassword
+          });
+          newuser.save(function(err) {
+            if (err) throw err;
+            else {
+              console.log('User is registered successfully!');
+              res.status(201).json({ message: 'Signup successful' });
+            }
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(500).json({ message: 'Enter a username and password' });
+  }
+});
+
 module.exports = router;
